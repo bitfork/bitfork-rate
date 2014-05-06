054af66081535c94d2fc54fbec12c9b2568e9fac
bitfork <bitfork.develop@gmail.com>
2014-05-06 14:32:26 +0400

    clear pid file error

--- protected/commands/WebsocketCommand.php
+++ protected/commands/WebsocketCommand.php
@@ -2,6 +2,33 @@
 
 class WebsocketCommand extends CConsoleCommand
 {
+	public function __construct()
+	{
+		register_shutdown_function(array($this, 'shutdown'));
+		Yii::app()->attachEventHandler('onError',array($this,'handleError'));
+		Yii::app()->attachEventHandler('onException',array($this,'handleError'));
+	}
+
+	public function handleError(CEvent $event)
+	{
+		if ($event instanceof CExceptionEvent) {
+			$this->actionStop();
+		}
+		elseif($event instanceof CErrorEvent) {
+			$this->actionStop();
+		}
+
+		$event->handled = false;
+	}
+
+	public function shutdown()
+	{
+		if ($error = error_get_last()) {
+			$this->actionStop();
+			Yii::app()->end();
+		}
+	}
+
 	public function actionStart()
     {
         $WebsocketServer = new WebsocketServer( (array) Yii::app()->websocket);
