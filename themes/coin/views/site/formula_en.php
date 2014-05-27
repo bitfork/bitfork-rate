7edf8fc7b60acc13055b028339a501932767ad8d
bitfork <bitfork.develop@gmail.com>
2014-05-27 15:50:57 +0400

    formula en

--- themes/coin/views/site/formula_en.php
+++ themes/coin/views/site/formula_en.php
@@ -1,4 +1,4 @@
-<h1>Формула рассчета битфорк индекса криптовалют</h1>
+<h1>Formula estimate crypto currency  price index</h1>
 <p>Bitfork-rate.com using price and volume from popular bitcoin, litecoin and other crypto currencies exchanges. We use only API service from Exchages.</p>
 
 
@@ -43,42 +43,42 @@
 <h3>Bitcoin to USD last price index calculation example.</h3>
 <h4>Initial data:</h4>
 <div class="main-bordered-double main-padding_10">
-	<p><b>Биржа 1:</b> <tt>price <sub>1</sub></tt> = 1500 USD,  <tt class="tt-light">Exchange_Volume <sub>1</sub></tt> = 20 000 BTC</p>
-	<p><b>Биржа 2:</b> <tt>price <sub>2</sub></tt> = 1490 USD,  <tt class="tt-light">Exchange_Volume <sub>2</sub></tt> = 5 000 BTC</p>
-	<p><b>Биржа 3:</b> <tt>price <sub>3</sub></tt> = 1550 USD,  <tt class="tt-light">Exchange_Volume <sub>3</sub></tt> = 25 000 BTC</p>
+	<p><b>Exchange 1:</b> <tt>price <sub>1</sub></tt> = 1500 USD,  <tt class="tt-light">Exchange_Volume <sub>1</sub></tt> = 20 000 BTC</p>
+	<p><b>Exchange 2:</b> <tt>price <sub>2</sub></tt> = 1490 USD,  <tt class="tt-light">Exchange_Volume <sub>2</sub></tt> = 5 000 BTC</p>
+	<p><b>Exchange 3:</b> <tt>price <sub>3</sub></tt> = 1550 USD,  <tt class="tt-light">Exchange_Volume <sub>3</sub></tt> = 25 000 BTC</p>
 </div>
-<h4>Пример расчета:</h4>
+<h4>Сalculation example:</h4>
 <div class="main-bordered-double main-padding_10">
-	<h5>Шаг 0 (вычисление Summary_Volume):</h5>
+	<h5>Step 0 (calculation Summary_Volume):</h5>
 	<p><tt>Summary_Volume</tt> = <tt class="tt-light">Exchange_Volume <sub>1</sub></tt> + <tt class="tt-light">Exchange_Volume <sub>2</sub></tt> + <tt class="tt-light">Exchange_Volume <sub>3</sub></tt></p>
 	<p><tt>Summary_Volume</tt> = 20 000 BTC + 5 000 BTC + 25 000 BTC = 50 000 BTC</p>
 </div>
 <div class="main-bordered-double main-padding_10 main-margTop">
-	<h5>Шаг 1 (вычисляем данные по первой бирже):</h5>
+	<h5>Step 1 (calculation data from Exchange 1):</h5>
 	<aside class="main-formula">
 		<tt>Bitfork index <sub>1</sub></tt> = $${\frac{\mathrm{20&nbsp;000&nbsp;BTC}}{\mathrm{50&nbsp;000&nbsp;BTC}} \times {\mathrm{1&nbsp;500&nbsp;USD}} \mathrm{&nbsp;=&nbsp;600}}$$
 	</aside>
 </div>
 <div class="main-bordered-double main-padding_10 main-margTop">
-	<h5>Шаг 2 (вычисляем данные по второй бирже): </h5>
+	<h5>Step 2 (calculation data from Exchange 1): </h5>
 	<aside class="main-formula">
 		<tt>Bitfork index <sub>2</sub></tt> = $${\frac{\mathrm{5&nbsp;000&nbsp;BTC}}{\mathrm{50&nbsp;000&nbsp;BTC}} \times {\mathrm{1&nbsp;490&nbsp;USD}} \mathrm{&nbsp;=&nbsp;149}}$$
 	</aside>
 </div>
 <div class="main-bordered-double main-padding_10 main-margTop">
-	<h5>Шаг 3 (вычисляем данные по третьей бирже):</h5>
+	<h5>Step 3 (calculation data from Exchange 3):</h5>
 	<aside class="main-formula">
 		<tt>Bitfork index <sub>3</sub></tt> = $${\frac{\mathrm{25&nbsp;000&nbsp;BTC}}{\mathrm{50&nbsp;000&nbsp;BTC}} \times {\mathrm{1&nbsp;550&nbsp;USD}} \mathrm{&nbsp;=&nbsp;775}}$$
 	</aside>
 </div>
 <div class="main-bordered-double main-padding_10 main-margTop">
-	<h5>Шаг 4 (вычисление индекса):</h5>
+	<h5>Step 4 (calculation Bitfork index):</h5>
 	<aside class="main-formula">
 		<tt>Bitfork index</tt> = $${\mathrm{600&nbsp;+&nbsp;149&nbsp;+&nbsp;775&nbsp;=&nbsp;1&nbsp;524&nbsp;USD}}$$
 	</aside>
 </div>
 <div class="main-bordered-double main-padding_10 main-margTop">
-	<h5>Итог вычисления:</h5>
+	<h5>Result:</h5>
 	<aside class="main-formula">
 		<tt>Bitfork index</tt> = 1&nbsp;524 USD
 	</aside>
@@ -87,19 +87,19 @@
 
 
 <div class="main-margTop2 text-center">
-	<h2>Начни использовать битфорк индекс уже сегодня</h2>
+	<h2>Start using bitfork index today</h2>
 	<div class="main-zoom">
 		<div class="main-col3">
 			<a class="imgWrap-140" href="#"><img alt="" src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/i/bitforkDevelop.jpg"/></a>
-			<h3><a href="#">Заказать интеграцию</a></h3>
+			<h3><a href="#">Order integration</a></h3>
 		</div>
 		<div class="main-col3">
 			<a class="imgWrap-140" href="#"><img alt="" src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/i/api.jpg"/></a>
-			<h3><a href="#">Интегрировать самостоятельно</a></h3>
+			<h3><a href="#">integrate themselves</a></h3>
 		</div>
 		<div class="main-col3">
 			<a class="imgWrap-140" href="https://github.com/bitfork/bitfork-rate"><img alt="" src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/i/github.jpg"/></a>
-			<h3><a href="#">Скачать исходники с GitHub</a></h3>
+			<h3><a href="#">Download the source from GitHub</a></h3>
 		</div>
 	</div>
 </div>
