df1991837ae27f6661fcf707326d451b7bdcfacc
bitfork <bitfork.develop@gmail.com>
2014-08-06 10:08:43 +0400

    fix notice

--- cron.php
+++ cron.php
@@ -2,7 +2,7 @@
 ini_set('memory_limit','128M');
 ini_set('date.timezone', 'Europe/Moscow');
 
-if ($_SERVER['HTTP_HOST']=='bitfork'){
+if (isset($_SERVER['windir'])) {
 	defined('YII_DEBUG') or define('YII_DEBUG',true);
 	require_once(dirname(__FILE__).'/../framework/yii.php');
 } else {
