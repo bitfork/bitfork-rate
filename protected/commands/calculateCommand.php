0b01c24e55485cc13eeeba4e0bb86d3468745fae
bitfork <bitfork.develop@gmail.com>
2014-08-15 16:17:50 +0400

    gearman

--- protected/commands/calculateCommand.php
+++ protected/commands/calculateCommand.php
@@ -2,13 +2,78 @@
 // cron
 class CalculateCommand extends CConsoleCommand
 {
-	public function actionRun($period = null)
+	private $_pidFile = 'calc.pid';
+
+	public function __construct()
 	{
-		$client= new GearmanClient();
-		$client->addServer('127.0.0.1', '4730');
-		$pairs = Pair::model()->findAll();
-		foreach ($pairs as $pair) {
-			$client->doBackground('calculate-rate', json_encode(array('period'=>$period, 'pair'=>$pair->id)));
+		$this->_pidFile = Yii::app()->getRuntimePath().'/'.$this->_pidFile;
+		register_shutdown_function(array($this, 'shutdown'));
+		Yii::app()->attachEventHandler('onError',array($this,'handleError'));
+		Yii::app()->attachEventHandler('onException',array($this,'handleError'));
+	}
+
+	public function handleError(CEvent $event)
+	{
+		if ($event instanceof CExceptionEvent) {
+			$this->stopProcess();
 		}
+		elseif($event instanceof CErrorEvent) {
+			$this->stopProcess();
+		}
+
+		$event->handled = false;
+	}
+
+	public function shutdown()
+	{
+		if ($error = error_get_last()) {
+			$this->stopProcess();
+			Yii::app()->end();
+		}
+	}
+
+	public function stopProcess()
+	{
+		@unlink($this->_pidFile);
+	}
+
+	public function startProcess()
+	{
+		@file_put_contents($this->_pidFile, '');
+	}
+
+	public function isProcess()
+	{
+		if (file_exists($this->_pidFile)) {
+			return true;
+		}
+		return false;
+	}
+
+	public function actionRun($pair = false, $period = null)
+	{
+		$this->_pidFile = str_replace('.pid', 'pair-'. $pair .'-'. $period .'.pid', $this->_pidFile);
+		if ($this->isProcess()) {
+			echo "процесс уже запущен";
+			Yii::app()->end();
+		}
+		$this->startProcess();
+
+		if (empty($period) and $period!==null) {
+			// если переид 0 то имитируем бесконечный цыкл с интервалом в 5 сек
+			$i = 0;
+			while ($i<Yii::app()->params['count_step_calculate']) {
+				// расчет индекса для каждой комбинации
+				Course::calculateIndex($pair, $period);
+				$i++;
+				sleep(Yii::app()->params['sleep_step_calculate']);
+			}
+		} else {
+			Course::calculateIndex($pair, $period);
+		}
+
+		echo "stop";
+
+		$this->stopProcess();
 	}
 }
