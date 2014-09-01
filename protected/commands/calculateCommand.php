f51a8624488a65039108647f841efefee91a6715
bitfork <bitfork.develop@gmail.com>
2014-09-01 16:09:31 +0500

    change calculate 0

--- protected/commands/calculateCommand.php
+++ protected/commands/calculateCommand.php
@@ -7,6 +7,21 @@ class CalculateCommand extends CConsoleCommand
 		$client= new GearmanClient();
 		$client->addServer('127.0.0.1', '4730');
 		$pairs = Pair::model()->findAll();
+
+		if ($period == 0) {
+			while (1) {
+				$this->doBackground($client, $period, $pairs);
+				sleep(Yii::app()->params['sleep_step_calculate']);
+			}
+		} else {
+			$this->doBackground($client, $period, $pairs);
+		}
+
+		return 0;
+	}
+
+	public function doBackground($client, $period, $pairs)
+	{
 		foreach ($pairs as $pair) {
 			$client->doBackground('calculate-rate', json_encode(array('period'=>$period, 'pair'=>$pair->id)));
 		}
