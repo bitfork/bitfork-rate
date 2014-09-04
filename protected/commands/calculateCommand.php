a3c2cddb36342cab99e705fa314170583d3c56de
bitfork <bitfork.develop@gmail.com>
2014-09-04 10:59:03 +0500

    change calculate high

--- protected/commands/calculateCommand.php
+++ protected/commands/calculateCommand.php
@@ -14,16 +14,20 @@ class CalculateCommand extends CConsoleCommand
 				sleep(Yii::app()->params['sleep_step_calculate']);
 			}
 		} else {
-			$this->doBackground($client, $period, $pairs);
+			$this->doBackground($client, $period, $pairs, true);
 		}
 
 		return 0;
 	}
 
-	public function doBackground($client, $period, $pairs)
+	public function doBackground($client, $period, $pairs, $high = false)
 	{
 		foreach ($pairs as $pair) {
-			$client->doBackground('calculate-rate', json_encode(array('period'=>$period, 'pair'=>$pair->id)));
+			if ($high===true) {
+				$client->doHighBackground('calculate-rate', json_encode(array('period'=>$period, 'pair'=>$pair->id)));
+			} else {
+				$client->doBackground('calculate-rate', json_encode(array('period'=>$period, 'pair'=>$pair->id)));
+			}
 		}
 	}
 }
